90a37a93b0c16c16126ae787d6f3d8327fc7afbe
Thomas Harlin <ThomasHarlin@users.noreply.github.com>
2021-03-07 21:31:47 -0500

    finalized pop art code

--- PopArtPCBFinal/PopArtPCBFinal.ino
+++ PopArtPCBFinal/PopArtPCBFinal.ino
@@ -28,9 +28,19 @@ int fade = 0;
 long sT;
 int timer;
 
-void setup() {
-  // put your setup code here, to run once:
 
+int whichFrame = 0;
+
+int colors [6][3] {
+  {255, 0, 0},
+  {255, 150, 0},
+  {255, 255, 0},
+  {0, 255, 0},
+  {0, 0, 255},
+  {60, 0, 255},
+};
+
+void setup() {
   strip.begin();
   strip.clear();
   strip.setBrightness(255);
@@ -43,15 +53,14 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   butt.update();
 
   if (butt.rose()) {
     stuff = (stuff + 1) % 4;
+    strip.clear();
+    strip.show();
+    sT = millis();
   }
-
-
 
   //state machine for the lights
   switch (stuff) {
@@ -69,35 +78,71 @@ void loop() {
       break;
   }
 }
-
-
-
-//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void offFunc() {
   strip.clear();
   strip.show();
 }
-//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void staticFunc() {
+  //makes the board look like it has a purple lable and black vinyl
   for (int i = 0; i < strip.numPixels(); i++) {
-    //white outside with colored center lable. Mybe follow the vinyl outside instead?
+    //pink record lable
+    if (i == 3 || i == 6 || i == 7 || i == 11 || i == 12 || i == 16 || i == 17 || i == 20) {
+      strip.setPixelColor(i, 255, 0, 255);
+    }
+    else{
+      strip.setPixelColor (i, 50,50,50);
+    }
   }
+  strip.show();
 }
-//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void simpleFunc() {
-  timer = 5;
+  //rainbow fade on the lable, rest is white
+  timer = 15;
 
-  if ((sT - millis()) > timer) {
-    for (int i = 0; i < strip.numPixels(); i++) {
+  for (int i = 0; i < strip.numPixels(); i++) {
+
+    //fade lable
+    if (i == 3 || i == 6 || i == 7 || i == 11 || i == 12 || i == 16 || i == 17 || i == 20) {
       strip.setPixelColor(i, Wheel(fade));
     }
+    else {
+      //white outside
+      strip.setPixelColor(i, 50, 50, 50);
+    }
+  }
+  if ((millis() - sT) > timer) {
     fade = (fade + 1) % 255;
     strip.show();
+    sT = millis();
   }
-  sT = millis();
+
 }
-//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void fancyFunc() {
   //make a rotating record animation, maybe have the lable in the middle color fade
+  timer = 100;
+
+  for (int i = 0; i < strip.numPixels(); i++) {
+
+    int pixAddress = pixelMap[i];
+    int whichColor = animation[whichFrame][i];
+
+    int red = colors[whichColor][0];
+    int green = colors[whichColor][1];
+    int blue = colors[whichColor][2];
+
+    if (pixAddress != -1) {
+      strip.setPixelColor(pixAddress, red, green, blue);
+    }
+  }
+
+  if (millis() - sT > timer) {
+    sT = millis();
+    whichFrame = (whichFrame + 1) % 6;
+    strip.show();
+  }
 }
-//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
